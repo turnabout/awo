42690657e5194a32d7c1c94c000f04932f495fba
Kevin Pageau <pageau.kevin@gmail.com>
2019-09-25 13:59:25 -0400

    Define tile selector inner rectangle width first
    * Changes logic so tile selector's inner rectangle gets
      defined first, followed by the outer rectangle.
    
      Done to more easily control the inner width of the selector

--- AWO/Src/Game/Editor/UI/selector.c
+++ AWO/Src/Game/Editor/UI/selector.c
@@ -12,9 +12,9 @@ typedef struct Tile_Selection {
 
 // UI element for selecting a tile to add.
 struct Tile_Selector {
-    int active;               // Whether the selector is currently active or not
-    SDL_Rect draw_rect;       // The tile selector's outer rectangle
-    SDL_Rect inner_draw_rect; // The tile selector's inner rectangle
+    int active;          // Whether the selector is currently active or not
+    SDL_Rect outer_rect; // The tile selector's outer rectangle
+    SDL_Rect inner_rect; // The tile selector's inner rectangle
     Tile_Selection sel_tiles[TILE_TYPE_BASIC_COUNT]; // Tile selections shown in selector
 };
 
@@ -25,55 +25,57 @@ Selector* tile_selector_create(Tiles_Data* td)
 
     sel->active = 1;
 
-    // Set outer and inner draw rectangles
-    SDL_Rect draw_rect = {
-        0 + SELECTOR_OUTER_PADDING, 
-        0 + SELECTOR_OUTER_PADDING, 
-        SELECTOR_WIDTH - (SELECTOR_OUTER_PADDING * 2), 
-        SELECTOR_HEIGHT - (SELECTOR_OUTER_PADDING * 2),
+    // Set inner and outer rectangles
+    SDL_Rect inner_rect = {
+        TILE_SELECTOR_X + TILE_SELECTOR_PADDING,
+        TILE_SELECTOR_Y + TILE_SELECTOR_PADDING,
+        TILE_SELECTOR_WIDTH,
+        0
     };
 
-    SDL_Rect inner_draw_rect = {
-        draw_rect.x + SELECTOR_INNER_PADDING,
-        draw_rect.y + SELECTOR_INNER_PADDING,
-        draw_rect.w - (SELECTOR_INNER_PADDING * 2),
-        draw_rect.h - (SELECTOR_INNER_PADDING * 2)
+    SDL_Rect outer_rect = {
+        TILE_SELECTOR_X,
+        TILE_SELECTOR_Y,
+        TILE_SELECTOR_WIDTH + (TILE_SELECTOR_PADDING * 2),
+        0
     };
-
-    sel->draw_rect = draw_rect;
-    sel->inner_draw_rect = inner_draw_rect;
 
     // Add tile frames
-    int cur_y = inner_draw_rect.y;
-    int tiles_w = inner_draw_rect.w;
-    int size_multiplier = tiles_w / DEFAULT_TILE_DIMENSION; // Multiplier to get tiles' height
+    int cur_y = inner_rect.y;
+    int sels_w = inner_rect.w;
+    int size_multiplier = sels_w / DEFAULT_TILE_DIMENSION; // Multiplier to get selections' height
 
     for (Tile_Type tt = TILE_TYPE_FIRST; tt < TILE_TYPE_BASIC_COUNT; tt++) {
+
+        // Get default variation for this tile type
         Tile_Var default_var = default_tile_vars[tt];
 
         // Add source rectangle
         SDL_Rect* src_rect = TD_gather_tile_data(td, tt, default_var, NULL, NULL);
 
-        // Add draw rectangle
+        // Add tile with draw rectangle
         Tile_Selection tile = {
             src_rect,
             {
-                inner_draw_rect.x, 
+                inner_rect.x, 
                 cur_y, 
-                tiles_w, 
+                sels_w, 
                 src_rect->h * size_multiplier
             }
         };
 
-        cur_y += tile.draw_rect.h + SELECTOR_INNER_PADDING;
+        cur_y += tile.draw_rect.h + TILE_SELECTOR_PADDING;
 
         sel->sel_tiles[tt] = tile;
     }
 
     // Close off the selector box
-    cur_y -= SELECTOR_INNER_PADDING;
-    sel->draw_rect.h = cur_y;
-    sel->inner_draw_rect.h = cur_y - (SELECTOR_INNER_PADDING * 2);
+    cur_y -= TILE_SELECTOR_PADDING;
+    outer_rect.h = cur_y;
+    inner_rect.h = cur_y - (TILE_SELECTOR_PADDING * 2);
+
+    sel->inner_rect = inner_rect;
+    sel->outer_rect = outer_rect;
 
     return sel;
 }
@@ -89,8 +91,8 @@ void tile_selector_draw(Selector* sel, SDL_Renderer* rend, SDL_Texture* tiles_te
     if (sel->active) {
 
         // Draw UI rectangle
-        SDL_SetRenderDrawColor(rend, 0, 0, 0, EDITOR_UI_ALPHA);
-        SDL_RenderFillRect(rend, &sel->draw_rect);
+        SDL_SetRenderDrawColor(rend, EDITOR_UI_R, EDITOR_UI_G, EDITOR_UI_B, EDITOR_UI_ALPHA);
+        SDL_RenderFillRect(rend, &sel->outer_rect);
 
         // Draw selections
         for (Tile_Type tt = TILE_TYPE_FIRST; tt < TILE_TYPE_BASIC_COUNT; tt++) {
