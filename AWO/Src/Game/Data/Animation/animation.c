7aebb1dbba27697e1ecfe0357bebc6d7052f3bbb
Kevin Pageau <pageau.kevin@gmail.com>
2019-10-28 13:39:12 -0400

    Move old animation code & adapt to opengl

---
+++ AWO/Src/Game/Data/Animation/animation.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cglm/cglm.h>
+
+#include "conf.h"
+#include "Game/Data/Animation/animation.h"
+
+void create_frame_from_JSON(Frame* frame_ptr, const cJSON* frame_JSON, mat4 projection)
+{
+    // Gather dimensions
+    float x = (float)cJSON_GetObjectItemCaseSensitive(frame_JSON, "x")->valueint;
+    float y = (float)cJSON_GetObjectItemCaseSensitive(frame_JSON, "y")->valueint;
+
+    float w = (cJSON_HasObjectItem(frame_JSON, "w"))
+                ? (float)cJSON_GetObjectItemCaseSensitive(frame_JSON, "w")->valueint
+                : (float)DEFAULT_TILE_DIMENSION;
+
+    float h = (cJSON_HasObjectItem(frame_JSON, "h"))
+                ? (float)cJSON_GetObjectItemCaseSensitive(frame_JSON, "h")->valueint
+                : (float)DEFAULT_TILE_DIMENSION;
+
+    frame_ptr->dimensions[0] = w;
+    frame_ptr->dimensions[1] = h;
+
+    vec4 top_left     = { x,     y,     0.0f, 1.0f };
+    vec4 top_right    = { x + w, y,     0.0f, 1.0f };
+    vec4 bottom_left  = { x,     y + h, 0.0f, 1.0f };
+    vec4 bottom_right = { x + w, y + h, 0.0f, 1.0f };
+
+    // Transform frame data into Normalized Device Coordinates
+    glm_mat4_mulv(projection, top_left, top_left);
+    glm_mat4_mulv(projection, top_right, top_right);
+    glm_mat4_mulv(projection, bottom_left, bottom_left);
+    glm_mat4_mulv(projection, bottom_right, bottom_right);
+
+    // Texture coordinates are mapped between 0,0 and 1,1.
+    // The above transformation leaves us with coordinates between -1,-1 and 1,1, so we need
+    // to transform them so they all fit between 0,0 and 1,1.
+    // Dividing each vertex by 2 and adding 0.5 does the trick.
+    // TODO: use glm funcs
+    top_left[0]     = (top_left[0] / 2) + 0.50f;
+    top_left[1]     = (top_left[1] / 2) + 0.50f;
+    top_right[0]    = (top_right[0] / 2) + 0.50f;
+    top_right[1]    = (top_right[1] / 2) + 0.50f;
+    bottom_left[0]  = (bottom_left[0] / 2) + 0.50f;
+    bottom_left[1]  = (bottom_left[1] / 2) + 0.50f;
+    bottom_right[0] = (bottom_right[0] / 2) + 0.50f;
+    bottom_right[1] = (bottom_right[1] / 2) + 0.50f;
+
+    glm_vec4_copy(top_left, frame_ptr->top_left);
+    glm_vec4_copy(top_right, frame_ptr->top_right);
+    glm_vec4_copy(bottom_left, frame_ptr->bottom_left);
+    glm_vec4_copy(bottom_right, frame_ptr->bottom_right);
+}
+
+Animation* create_animation_from_JSON(const cJSON* animation_JSON, mat4 projection)
+{
+    Animation* animation = malloc(sizeof(Animation));
+
+    animation->count = cJSON_GetArraySize(animation_JSON);
+    Frame* frames = (Frame*)malloc( sizeof(Frame) * animation->count );
+
+    // Gather data from every frame and populate the allocated frames
+    cJSON* frame_JSON = NULL;
+    cJSON_ArrayForEach(frame_JSON, animation_JSON)
+    {
+        create_frame_from_JSON(frames, frame_JSON, projection);
+        frames++;
+    }
+
+    animation->frames = frames - animation->count;
+    return animation;
+
+}
+
+void free_animation(Animation* animation)
+{
+    // TODO: free every frame
+
+    free(animation);
+}
+
+#ifdef _DEBUG
+void print_animation(Animation* anim)
+{
+    printf("\n[\n");
+
+    // TODO
+    /*
+    for (int i = 0; i < anim->count; i++) {
+        printf(
+            "{x: %d, y: %d, w: %d, h: %d},\n", 
+            anim->frames[i].x,
+            anim->frames[i].y,
+            anim->frames[i].w,
+            anim->frames[i].h
+        );
+    }
+    */
+
+    printf("]\n");
+}
+#endif
---
+++ AWO/Src/Game/Data/Animation/animation.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cglm/types.h>
+#include <cJSON.h>
+
+/*! @brief Frame object.
+ *
+ * Describes the source data of a frame within the sprite sheet.
+ */
+typedef struct Frame {
+    vec4 top_left;     // This frame's source coordinates, in Normalized Device Coordinates
+    vec4 top_right;    // Indexes go x/y/w/h
+    vec4 bottom_left;
+    vec4 bottom_right;
+
+    vec2 dimensions;  // Width/height of this frame. Indexes go w/h
+} Frame;
+
+/*! @brief Animation object.
+ *
+ * Contains a reference to the first of many frames making up an animation.
+ */
+typedef struct Animation {
+    int count;
+    Frame* frames;
+} Animation;
+
+/*! @brief Creates an animation struct from the given cJSON animation array object.
+ *
+ *  @param[in] animation_json The JSON representing this animation and its frames.
+ *  @param[in] projection The projection matrix used to convert frame source coordinates 
+ *  to NDC.
+ */
+Animation* create_animation_from_JSON(const cJSON* animation_JSON, mat4 projection);
+
+// 
+/*! @brief Frees an animation's allocated memory.
+ *
+ *  @param[in] animation The animation to free.
+ */
+void free_animation(Animation* animation);
+
+/*! @brief Gets pointer to the first frame in an Animation.
+ *
+ *  @param[in] animation The animation object from which to fetch the frame.
+ *  @param[out] count Filled in with the frame count in the animation.
+ */
+// Frame* get_animation_frames(Animation* animation, int* count);
+
+#ifdef _DEBUG
+// 
+/*! @brief Prints an Animation's contents.
+ *
+ *  @param[in] animation The animation object to print.
+ */
+void print_animation(Animation* animation);
+#endif
