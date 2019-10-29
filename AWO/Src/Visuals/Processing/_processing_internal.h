56357db56189a6f84a6eec71d094e368640f4479
Kevin Pageau <pageau.kevin@gmail.com>
2019-10-29 14:44:55 -0400

    Delete all dead code from old implementation

--- AWO/Src/Visuals/Processing/_processing_internal.h
+++
@@ -1,5 +0,0 @@
-#include <SDL.h>
-#include "Visuals/Data/Palette/palette_tree.h"
-
-// Apply the given palette to the current texture target of the given renderer
-SDL_Texture* apply_palette(SDL_Renderer* rend, Palette_Tree* palette, int w, int h);
--- AWO/Src/Visuals/visuals_data.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "Data/Units/units_data.h"
-#include "Data/Tiles/tiles_data.h"
