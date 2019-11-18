ab66cdab1025cbcf110db48b029009c2226b6869
Kevin Pageau <pageau.kevin@gmail.com>
2019-11-18 13:41:47 -0500

    Finish comment specifying game palette indexes

--- AWO/Src/Game/Data/Palette/raw_palette.h
+++ AWO/Src/Game/Data/Palette/raw_palette.h
@@ -10,12 +10,35 @@
 
 /*! @brief Texture containing every row of palette data that can be used by the game.
  *  Structure:
- *   0: Clear
- *   1: Clear (Fog)
- *   2: Snow
- *   3: Snow (Fog)
- *   4: Rain
- *   5: Rain (Fog)
+ *   0: Units: OS
+ *   1: Units: OS (Done)
+ *   2: Units: BM
+ *   3: Units: BM (Done)
+ *   4: Units: GE
+ *   5: Units: GE (Done)
+ *   6: Units: YC
+ *   7: Units: YC (Done)
+ *   8: Units: BH
+ *   9: Units: BH (Done)
+ *  10: Clear (Fog)
+ *  11: Clear
+ *  12: Clear (Fog)
+ *  13: Snow
+ *  14: Snow (Fog)
+ *  15: Rain
+ *  16: Rain (Fog)
+ *  17: Properties: OS
+ *  18: Properties: OS (Fog)
+ *  19: Properties: BM
+ *  20: Properties: BM (Fog)
+ *  21: Properties: GE
+ *  22: Properties: GE (Fog)
+ *  23: Properties: YC
+ *  24: Properties: YC (Fog)
+ *  25: Properties: BH
+ *  26: Properties: BH (Fog)
+ *  27: Properties: Neutral
+ *  28: Properties: Neutral (Fog)
  */
 
 /*! @brief Creates the main palette texture used to color swap sprites.
