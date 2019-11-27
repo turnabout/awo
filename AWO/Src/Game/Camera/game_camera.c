d89c354fb4e446641825eb9cc4d894ad7c6e9e15
Kevin Pageau <pageau.kevin@gmail.com>
2019-11-27 02:28:22 -0500

    Add functions for updating the game camera

--- AWO/Src/Game/Camera/game_camera.c
+++ AWO/Src/Game/Camera/game_camera.c
@@ -5,6 +5,10 @@
 #include "Game/Camera/game_camera.h"
 #include "Game/Renderer/game_renderer.h"
 
+#define DEFAULT_CAMERA_ZOOM 2.0f
+#define DEFAULT_CAMERA_X 0.0f
+#define DEFAULT_CAMERA_Y 0.0f
+
 struct Game_Camera {
 
     // Identity matrix reference.
@@ -16,6 +20,12 @@ struct Game_Camera {
     // The game's projection matrix.
     mat4 projection;
 
+    // Current zoom value.
+    float zoom;
+
+    // Current pan coordinate values.
+    float x, y;
+
 };
 
 Game_Camera* create_game_camera(int window_width, int window_height)
@@ -24,38 +34,66 @@ Game_Camera* create_game_camera(int window_width, int window_height)
 
     glm_mat4_identity(camera->identity);
 
-    // Set starting view matrix
-    glm_scale_to(
-        camera->identity,
-        (vec3) { (GLfloat)DEFAULT_TILE_SIZE * 2, (GLfloat)DEFAULT_TILE_SIZE * 2, 1.0f },
-        camera->view
-    );
-
     // Set starting projection matrix
-    // glm_mat4_identity(camera->projection);
-    glm_ortho(
-        0.0f, 
-        (float)window_width, 
-        0.0f, 
-        (float)window_height, 
-        -1.0f, 
-        1.0f, 
-        camera->projection
-    );
-
+    glm_ortho(0.0f, (float)window_width, 0.0f, (float)window_height, -1.0f, 1.0f, camera->projection);
     update_game_renderer_matrix(camera->projection, "projection");
-    update_game_renderer_matrix(camera->view, "view");
+
+    // Set starting view matrix
+    camera->x = DEFAULT_CAMERA_X;
+    camera->y = DEFAULT_CAMERA_Y;
+
+    update_game_camera_view_zoom_to(camera, DEFAULT_CAMERA_ZOOM);
 
     return camera;
 }
 
-void update_game_camera_view_position(Game_Camera* camera, float added_x, float added_y)
+void _refresh_game_camera(Game_Camera* camera)
 {
+    // Refresh position
+    glm_translate_to(
+        camera->identity,
+        (vec3) { camera->x, camera->y, 0.0f },
+        camera->view
+    );
 
+    // Refresh zoom
+    glm_scale(
+        camera->view,
+        (vec3) { 
+            (GLfloat)DEFAULT_TILE_SIZE * camera->zoom, 
+            (GLfloat)DEFAULT_TILE_SIZE * camera->zoom, 
+            1.0f 
+        }
+    );
+
+    // Update other modules depending on the camera
+    update_game_renderer_matrix(camera->view, "view");
 }
 
-void update_game_camera_view_zoom(Game_Camera* camera, float added_value)
+void update_game_camera_view_position_to(Game_Camera* camera, float x, float y)
 {
+    camera->x = x;
+    camera->y = y;
+    _refresh_game_camera(camera);
+}
+
+void update_game_camera_view_position(Game_Camera* camera, float added_x, float added_y)
+{
+    camera->x += added_x;
+    camera->y += added_y;
+    _refresh_game_camera(camera);
+}
+
+void update_game_camera_view_zoom(Game_Camera* camera, float added_zoom)
+{
+    camera->zoom += added_zoom;
+    _refresh_game_camera(camera);
+}
+
+void update_game_camera_view_zoom_to(Game_Camera* camera, float zoom)
+{
+    camera->zoom = zoom;
+    _refresh_game_camera(camera);
 }
 
 void free_game_camera(Game_Camera* camera)
--- AWO/Src/Game/Camera/game_camera.h
+++ AWO/Src/Game/Camera/game_camera.h
@@ -9,7 +9,9 @@ typedef struct Game_Camera Game_Camera;
 
 Game_Camera* create_game_camera(int window_width, int window_height);
 
+void update_game_camera_view_position_to(Game_Camera* camera, float x, float y);
 void update_game_camera_view_position(Game_Camera* camera, float added_x, float added_y);
-void update_game_camera_view_zoom(Game_Camera* camera, float added_value);
+void update_game_camera_view_zoom(Game_Camera* camera, float added_zoom);
+void update_game_camera_view_zoom_to(Game_Camera* camera, float zoom);
 
 void free_game_camera(Game_Camera* camera);
