01636dcfa570cd547c4ef72e05d89e1aeb27c177
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-10 11:43:49 -0500

    Fix emscripten broken include

--- AWO/Src/Emscripten_Exports/emx_editor.c
+++ AWO/Src/Emscripten_Exports/emx_editor.c
@@ -1,7 +1,7 @@
 #include "conf.h"
 #include "Emscripten_Exports/emx.h"
 #include "Game/_game.h"
-#include "Game/Editor/editor.h"
+#include "Game/Editor/game_editor.h"
 #include "Game/Data/Tile/tiles_data.h"
 
 // Gets the next basic tile type string. Must be called continuously until it returns NULL.
