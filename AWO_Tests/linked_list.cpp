b9b2edea12d31efe54ba99c9b3605d91db294adf
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-11 08:47:56 -0500

    Add google test project & basic test usage

---
+++ AWO_Tests/linked_list.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "../AWO/Src/Utilities/Linked_List/linked_list.h"
+
+class Linked_List_Test : public ::testing::Test {
+    protected:
+    void SetUp() override {
+        value = 1;
+    }
+
+    // void TearDown() override {}
+
+    int value = 0;
+};
+
+TEST_F(Linked_List_Test, create_linked_list)
+{
+    Linked_List* list = NULL;
+
+    EXPECT_TRUE(list == NULL);
+    EXPECT_EQ(value, 1) << "Error";
+    EXPECT_TRUE(true);
+}
---
+++ AWO_Tests/pch.h
@@ -0,0 +1,10 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include "gtest/gtest.h"
+
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
