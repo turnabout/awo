928323512864a7573b42d997a15d07fe15ad36f7
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-12 03:37:25 -0500

    Add linked list creation test

--- AWO_Tests/linked_list.cpp
+++ AWO_Tests/linked_list.cpp
@@ -13,24 +13,37 @@ class Linked_List_Test : public ::testing::Test {
     // void TearDown() override {}
 
     int value = 0;
+    Linked_List* filled_list;
+    Linked_List* empty_list;
 };
 
+void loop_empty_linked_list(void* element, void* additional_value)
+{
+    FAIL() << "List is not empty";
+}
+
+void loop_filled_linked_list(void* element, void* calculated_sum)
+{
+    *((int*)calculated_sum) += *((int*)element);
+}
 TEST_F(Linked_List_Test, create_linked_list)
 {
+    // Create a linked list with no values
     Linked_List* list = create_linked_list(NULL, 0);
 
-    EXPECT_TRUE(list != NULL);
+    loop_linked_list(list, loop_empty_linked_list, NULL);
+    free_linked_list(list);
 
-    /*
+    // Create a linked list with values
     int a = 1, b = 2, c = 3, d = 4;
     int* values[4] = {&a, &b, &c, &d};
+    int element_count = a + b + c + d;
+    int expected_element_count = 0;
 
-    Linked_List* list = create_linked_list((void**)values, 4);
+    list = create_linked_list((void**)values, 4);
 
+    loop_linked_list(list, loop_filled_linked_list, &expected_element_count);
 
-    EXPECT_TRUE(list == NULL);
-    EXPECT_TRUE(list == NULL);
-    EXPECT_EQ(value, 1) << "Error";
-    EXPECT_TRUE(true);
-    */
+    EXPECT_EQ(expected_element_count, element_count);
+    free_linked_list(list);
 }
