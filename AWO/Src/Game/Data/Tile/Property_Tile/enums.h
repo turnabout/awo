ec577211df8fb6d80d9b815fbbcec1a727d428c6
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-13 18:31:37 -0500

    Remove unneeded property tile enum

--- AWO/Src/Game/Data/Tile/Property_Tile/enums.h
+++ AWO/Src/Game/Data/Tile/Property_Tile/enums.h
@@ -16,15 +16,3 @@ static const char* property_type_str[] = {FOREACH_PROPERTY_TYPE(GENERATE_STRING)
 #define PROPERTY_TYPE_FIRST HQ
 #define PROPERTY_TYPE_LAST Port
 #define PROPERTY_TYPE_COUNT PROPERTY_TYPE_LAST + 1
-
-// Property Weather Variations
-#define FOREACH_PROPERTY_VARIATION(PROPERTY_VARIATION) \
-    PROPERTY_VARIATION(Property_Clear) \
-    PROPERTY_VARIATION(Property_Snow) \
-
-typedef enum {FOREACH_PROPERTY_VARIATION(GENERATE_ENUM)} Property_Variation;
-static const char* property_variation_str[] = {FOREACH_PROPERTY_VARIATION(GENERATE_STRING)};
-
-#define PROPERTY_VARIATION_FIRST Property_Clear
-#define PROPERTY_VARIATION_LAST  Property_Snow
-#define PROPERTY_VARIATION_COUNT PROPERTY_VARIATION_LAST + 1
