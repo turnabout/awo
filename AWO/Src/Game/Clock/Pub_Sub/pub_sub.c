3343dd10b1bf3c9347b22f36b8b1585f8601e127
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-13 09:02:36 -0500

    Rename pub-sub function & update its documentation

--- AWO/Src/Game/Clock/Pub_Sub/pub_sub.c
+++ AWO/Src/Game/Clock/Pub_Sub/pub_sub.c
@@ -4,7 +4,7 @@
 
 struct Game_Clock_Pub_Sub {
 
-    // List of subscriber modules registered to the pub-sub service.
+    // List of subscriber modules registered to the pub-sub.
     void* subscriber_modules[ANIMATION_CLOCK_COUNT];
 
     // List of all subscribers' callback functions.
@@ -16,39 +16,39 @@ void game_clock_pub_sub_nop(Tick_Event* tick_event, void* module){}
 
 Game_Clock_Pub_Sub* create_clock_pub_sub()
 {
-    Game_Clock_Pub_Sub* service = (Game_Clock_Pub_Sub*)malloc(
+    Game_Clock_Pub_Sub* pub_sub = (Game_Clock_Pub_Sub*)malloc(
         sizeof(Game_Clock_Pub_Sub)
     );
 
     // Initialize all subscriber modules & callbacks to empty values
     for (int i = 0; i < ANIMATION_CLOCK_COUNT; i++) {
-        service->subscriber_modules[i] = NULL;
-        service->subscriber_callbacks[i] = game_clock_pub_sub_nop;
+        pub_sub->subscriber_modules[i] = NULL;
+        pub_sub->subscriber_callbacks[i] = game_clock_pub_sub_nop;
     }
 
-    return service;
+    return pub_sub;
 }
 
 void register_clock_pub_sub_subscriber(
-    Game_Clock_Pub_Sub* service,
+    Game_Clock_Pub_Sub* pub_sub,
     void* subscriber_module,
     clock_subscriber_event_cb subscriber_callback, 
     Animation_Clock_Index animation_clock
 )
 {
-    service->subscriber_modules[animation_clock] = subscriber_module;
-    service->subscriber_callbacks[animation_clock] = subscriber_callback;
+    pub_sub->subscriber_modules[animation_clock] = subscriber_module;
+    pub_sub->subscriber_callbacks[animation_clock] = subscriber_callback;
 }
 
-void register_clock_pub_sub_tick_event(Game_Clock_Pub_Sub* service, Tick_Event* tick_event)
+void process_clock_pub_sub_tick_event(Game_Clock_Pub_Sub* pub_sub, Tick_Event* tick_event)
 {
-    service->subscriber_callbacks[tick_event->clock_index](
+    pub_sub->subscriber_callbacks[tick_event->clock_index](
         tick_event,
-        service->subscriber_modules[tick_event->clock_index]
+        pub_sub->subscriber_modules[tick_event->clock_index]
     );
 }
 
-void free_clock_pub_sub(Game_Clock_Pub_Sub* service)
+void free_clock_pub_sub(Game_Clock_Pub_Sub* pub_sub)
 {
-    free(service);
+    free(pub_sub);
 }
