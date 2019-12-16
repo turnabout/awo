7dab4a842b2346af9d6e7fd88d2b57cf3e1219ea
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-16 18:38:00 -0500

    Add all COs to CO enum

--- AWO/Src/Game/Data/CO/enums.h
+++ AWO/Src/Game/Data/CO/enums.h
@@ -6,13 +6,22 @@
     CO(Andy) \
     CO(Max) \
     CO(Sami) \
+    CO(Nell) \
+    CO(Hachi) \
     CO(Olaf) \
     CO(Grit) \
+    CO(Colin) \
     CO(Eagle) \
     CO(Drake) \
+    CO(Jess) \
     CO(Kanbei) \
     CO(Sonja) \
+    CO(Sensei) \
     CO(Flak) \
+    CO(Adder) \
+    CO(Lash) \
+    CO(Hawke) \
+    CO(Sturm) \
 
 typedef enum {FOREACH_CO_TYPE(GENERATE_ENUM)} CO_Type;
 static const char* co_type_str[] = {FOREACH_CO_TYPE(GENERATE_STRING)};
