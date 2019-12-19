7ab1eb83549cde34715f5a53c8a476115c7d6d03
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-19 16:58:50 -0500

    Add units update callback functions implementation

--- AWO/Src/Game/Entity/Unit/update_unit_palette.c
+++ AWO/Src/Game/Entity/Unit/update_unit_palette.c
@@ -1,5 +1,16 @@
 #include "Game/Entity/Unit/_unit.h"
+#include "Game/Data/Palette/game_palette.h"
 
 void update_unit_palette(Game_Renderer* renderer, void* unit, Bool done)
 {
+    update_tile_layer_pixel_high(
+        renderer,
+        UNIT_LAYER,
+        ((Unit*)unit)->x,
+        ((Unit*)unit)->y,
+        (vec2) {
+            get_player_unit_palette_index(((Unit*)unit)->player->index, done),
+            0.0f
+        }
+    );
 }
