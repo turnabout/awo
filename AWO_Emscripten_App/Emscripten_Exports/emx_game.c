9d2516683d6706ea179b953d584bbddaf375b612
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-21 11:52:41 -0500

    Split emx exports
    * Initialization functions go into emx_initialization.c
    * Game updating functions go into emx_game.c
    * Editor-related functions go into emx_editor.c

--- AWO_Emscripten_App/Emscripten_Exports/emx_game.c
+++ AWO_Emscripten_App/Emscripten_Exports/emx_game.c
@@ -1,29 +1,7 @@
-#include "conf.h"
+#include <stdio.h>
+
 #include "Game/_game.h"
 #include "Emscripten_Exports/emx.h"
-
-// Initializes the game.
-Game* EMX init_AWO(int w, int h)
-{
-    Game* game;
-
-    if ((game = init_game(
-        DEFAULT_WINDOW_WIDTH,
-        DEFAULT_WINDOW_HEIGHT
-    )) == NULL) {
-        printf("Failed to initialize game\n");
-        exit_game(game);
-        return NULL;
-    }
-
-    return game;
-}
-
-// Runs the game.
-void EMX run_AWO(Game* game)
-{
-    emscripten_set_main_loop_arg(game_loop, game, FPS, 1);
-}
 
 // Updates the game's viewport dimensions.
 void EMX update_game_size(Game* game, int width, int height)
---
+++ AWO_Emscripten_App/Emscripten_Exports/emx_initialization.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "conf.h"
+#include "Game/_game.h"
+#include "Emscripten_Exports/emx.h"
+
+// Initializes the game.
+Game* EMX init_AWO(int w, int h)
+{
+    Game* game;
+
+    if ((game = init_game(
+        DEFAULT_WINDOW_WIDTH,
+        DEFAULT_WINDOW_HEIGHT
+    )) == NULL) {
+        printf("Failed to initialize game\n");
+        exit_game(game);
+        return NULL;
+    }
+
+    return game;
+}
+
+// Runs the game.
+void EMX run_AWO(Game* game)
+{
+    emscripten_set_main_loop_arg(game_loop, game, FPS, 1);
+}
