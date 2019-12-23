6c79b78fdf6d382aa94216bc4dfd3f81b6fa4dcf
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-23 06:38:00 -0500

    Add AWO API functions for getting some tiles data

--- AWO/Include/game_data.h
+++ AWO/Include/game_data.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "game.h"
+
+/*! @brief Gets the next neutral tile type.
+ *
+ *  @note Must be called continuously until it returns NULL.
+ *
+ *  @param[out] type Filled in with the neutral tile type's value.
+ *  @return The tile type's string.
+ */
+const char* get_next_game_tile_type_data(int* type);
+
+/*! @brief Gets the next neutral tile variation for a type.
+ *
+ *  @note Must be called continuously until it returns NULL.
+ *
+ *  @param[in] game The game instance.
+ *  @param[in] type The type from which to get the next variation.
+ *  @param[out] var Filled in with the neutral tile variation's value.
+ *  @return The tile variation's string.
+ */
+const char* get_next_game_tile_var_data(Game* game, int type, char* var);
---
+++ AWO/Src/API/Game_Data/tiles_data.c
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Game/_game.h"
+
+const char* get_next_game_tile_type(int* type_out)
+{
+    Tile_Type type = get_next_basic_tile_type();
+
+    if (type == TILE_TYPE_NONE) {
+        return NULL;
+    }
+
+    *type_out = type;
+    return tile_type_str[type];
+}
+
+const char* get_next_game_tile_variation(Game* game, int type, char* var_out)
+{
+    Tile_Variation variation = get_next_tile_variation(game->data->tiles, type);
+
+    *var_out = variation;
+
+    if (variation == TILE_VAR_NONE) {
+        return NULL;
+    }
+
+    return tile_var_str[variation];
+}
