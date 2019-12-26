fccfa2594173641903f7d29512d2b790b490a4eb
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-26 00:22:06 -0500

    Initialize new clock data structure

--- AWO/Src/Game/Data/Clock/clock_data.c
+++ AWO/Src/Game/Data/Clock/clock_data.c
@@ -7,59 +7,46 @@ PRAGMA( warning(disable: 6001) );
 
 typedef struct Animation_Clock_Data {
 
+    // Changing frames
+    Uint8* frames;
 
-
-    // Changing ticks
-    int* changing_ticks;
-    int changing_ticks_count;
-
-    // Child sub-clocks
-    Uint8** sub_clocks_tick_arrays;
-    int sub_clock_count;
+    // Corresponding values
+    Uint8* values;
+    Uint8 value_count;
 
 } Animation_Clock_Data;
 
 struct Clock_Data {
-
-    // Data for each animation clock.
     Animation_Clock_Data* clocks[ANIMATION_CLOCK_COUNT];
-
 };
 
-Animation_Clock_Data* create_animation_clock_data(cJSON* JSON)
+Animation_Clock_Data* create_animation_clock_data(cJSON* clock_JSON)
 {
-    /*
     Animation_Clock_Data* data = malloc(sizeof(Animation_Clock_Data));
 
-    // Store changing ticks data
-    cJSON* changing_ticks_JSON = cJSON_GetObjectItemCaseSensitive(JSON, "changingTicks");
+    cJSON* frames_JSON = cJSON_GetObjectItemCaseSensitive(clock_JSON, "frames");
+    cJSON* values_JSON = cJSON_GetObjectItemCaseSensitive(clock_JSON, "values");
 
-    data->changing_ticks_count = cJSON_GetArraySize(changing_ticks_JSON);
-    data->changing_ticks = malloc(sizeof(int) * data->changing_ticks_count);
+    data->value_count = cJSON_GetArraySize(values_JSON);
 
-    for (int i = 0; i < data->changing_ticks_count; i++) {
-        data->changing_ticks[i] = cJSON_GetArrayItem(changing_ticks_JSON, i)->valueint;
+    // Frame count should be the value count + 1 (last frame resets to first frame/value)
+    if (cJSON_GetArraySize(frames_JSON) != (data->value_count + 1)) {
+        free(data);
+        // TODO: error
+        return NULL;
     }
 
-    // Store sub clock data
-    cJSON* sub_clocks_JSON = cJSON_GetObjectItemCaseSensitive(JSON, "subClocks");
+    // Store frames & values
+    data->frames = malloc(sizeof(Uint8) * data->value_count + 1);
+    data->values = malloc(sizeof(Uint8) * data->value_count);
 
-    data->sub_clock_count = cJSON_GetArraySize(sub_clocks_JSON);
-    data->sub_clocks_tick_arrays = malloc(sizeof(Uint8*) * data->sub_clock_count);
-
-    for (int i = 0; i < data->sub_clock_count; i++) {
-        data->sub_clocks_tick_arrays[i] = malloc(sizeof(Uint8) * data->changing_ticks_count);
-
-        cJSON* sub_clock_JSON = cJSON_GetArrayItem(sub_clocks_JSON, i);
-
-        for (int j = 0; j < data->changing_ticks_count; j++) {
-            data->sub_clocks_tick_arrays[i][j] = cJSON_GetArrayItem(sub_clock_JSON, j)->valueint;
-        }
+    for (int i = 0; i < data->value_count; i++) {
+        data->values[i] = cJSON_GetArrayItem(values_JSON, i)->valueint;
+        data->frames[i] = cJSON_GetArrayItem(frames_JSON, i)->valueint;
     }
 
+    data->frames[data->value_count] = cJSON_GetArrayItem(frames_JSON, data->value_count)->valueint;
     return data;
-    */
-    return NULL;
 }
 
 Clock_Data* create_clock_data(cJSON* JSON)
@@ -85,24 +72,13 @@ void free_clock_data(Clock_Data* clock_data)
             continue;
         }
 
-        /*
-        Animation_Clock_Data* anim_clock = clock_data->clocks[i];
-
-        // Free animation clock's sub-clocks
-        for (int j = 0; j < anim_clock->sub_clock_count; j++) {
-            free(anim_clock->sub_clocks_tick_arrays[j]);
+        if (clock_data->clocks[i]->frames != NULL) {
+            free(clock_data->clocks[i]->frames);
         }
 
-        // Free animation clock
-        if (anim_clock->changing_ticks != NULL) {
-            free(anim_clock->changing_ticks);
+        if (clock_data->clocks[i]->values != NULL) {
+            free(clock_data->clocks[i]->values);
         }
-
-        if (anim_clock->sub_clocks_tick_arrays != NULL) {
-            free(anim_clock->sub_clocks_tick_arrays);
-        }
-        */
-
 
         free(clock_data->clocks[i]);
     }
