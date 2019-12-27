ce9a74708f38bf5e0b57c6360430be53f2b7b64a
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-27 14:26:37 -0500

    Add more tile values to default custom stage

--- AWO/Src/Game/Stage/generate_custom_stage.c
+++ AWO/Src/Game/Stage/generate_custom_stage.c
@@ -27,17 +27,23 @@ Stage* generate_custom_stage()
     edit_stage_tile(stage, 1, 2, Property_Base, Player_Index_0);
 
     // Sea
-    edit_stage_tile(stage, 3, 2, Sea, TopLeft);
+    edit_stage_tile(stage, 2, 2, Sea, TopLeft);
+    edit_stage_tile(stage, 3, 2, Shore, Top);
     edit_stage_tile(stage, 4, 2, Sea, Top);
-    edit_stage_tile(stage, 5, 2, Sea, TopRight);
+    edit_stage_tile(stage, 5, 2, Shore, TopConnectedRight);
+    edit_stage_tile(stage, 6, 2, Shore, TopRightConnectedHorizontal);
 
-    edit_stage_tile(stage, 3, 3, Sea, Left);
-    edit_stage_tile(stage, 4, 3, Sea, Middle);
-    edit_stage_tile(stage, 5, 3, Sea, Right);
+    edit_stage_tile(stage, 2, 3, Sea, Left);
+    edit_stage_tile(stage, 3, 3, Sea, Middle);
+    edit_stage_tile(stage, 4, 3, Reef, Default);
+    edit_stage_tile(stage, 5, 3, Sea, Middle);
+    edit_stage_tile(stage, 6, 3, Sea, Right);
 
-    edit_stage_tile(stage, 3, 4, Sea, BottomLeft);
+    edit_stage_tile(stage, 2, 4, Sea, BottomLeft);
+    edit_stage_tile(stage, 3, 4, Sea, Bottom);
     edit_stage_tile(stage, 4, 4, Sea, Bottom);
-    edit_stage_tile(stage, 5, 4, Sea, BottomRight);
+    edit_stage_tile(stage, 5, 4, Sea, Bottom);
+    edit_stage_tile(stage, 6, 4, Sea, BottomRight);
 
     // Some roads & properties
     edit_stage_tile(stage, 7, 2, Road, TopLeft);
@@ -53,6 +59,7 @@ Stage* generate_custom_stage()
     edit_stage_tile(stage, 7, 4, Road, Vertical);
 
     edit_stage_tile(stage, 3, 5, Silo, Default);
+    edit_stage_tile(stage, 3, 7, Sea, Hole);
     edit_stage_tile(stage, 4, 5, Property_City, Player_Index_Neutral);
     edit_stage_tile(stage, 5, 5, Road, Horizontal);
     edit_stage_tile(stage, 6, 5, Road, Horizontal);
@@ -63,6 +70,18 @@ Stage* generate_custom_stage()
     edit_stage_tile(stage, 7, 6, Road, Vertical);
 
     edit_stage_tile(stage, 7, 7, Property_Base, Player_Index_1);
+
+    // River
+    edit_stage_tile(stage, 6, 6, River, Horizontal);
+    edit_stage_tile(stage, 5, 6, River, TopLeft);
+    edit_stage_tile(stage, 5, 7, River, Vertical);
+    edit_stage_tile(stage, 5, 8, River, Vertical);
+    edit_stage_tile(stage, 5, 9, River, BottomLeft);
+    edit_stage_tile(stage, 6, 9, River, Horizontal);
+    edit_stage_tile(stage, 6, 9, River, Horizontal);
+    edit_stage_tile(stage, 7, 9, River, Horizontal);
+    edit_stage_tile(stage, 8, 9, River, Horizontal);
+
 
     return stage;
 }
