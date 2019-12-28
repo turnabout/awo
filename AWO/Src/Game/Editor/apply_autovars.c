e35e268e2d17b99f179669d06cbc442dd9782ed1
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-28 13:38:49 -0500

    Print auto-var attempt

--- AWO/Src/Game/Editor/apply_autovars.c
+++ AWO/Src/Game/Editor/apply_autovars.c
@@ -10,21 +10,22 @@ void apply_autovar(Game_Editor* editor, Game_Board* board, Game_Clock* clock, Ui
         return;
     }
 
-    edit_game_board_tile(
-        board,
-        clock,
+    Tile_Variation new_var = get_tile_auto_var(
+        editor->tiles_data,
         middle_tile_type,
-
-        get_tile_auto_var(
-            editor->tiles_data,
-            middle_tile_type,
-            get_game_board_tile_type(board, x,     y - 1),
-            get_game_board_tile_type(board, x + 1, y),
-            get_game_board_tile_type(board, x,     y + 1),
-            get_game_board_tile_type(board, x - 1, y)
-        ),
-
-        x,
-        y
+        get_game_board_tile_type(board, x, y - 1),
+        get_game_board_tile_type(board, x + 1, y),
+        get_game_board_tile_type(board, x, y + 1),
+        get_game_board_tile_type(board, x - 1, y)
     );
+
+    printf(
+        "Applying auto-var to %s (%d, %d): %s\n", 
+        tile_type_str[middle_tile_type],
+        x,
+        y,
+        tile_var_str[new_var]
+    );
+
+    edit_game_board_tile(board, clock, middle_tile_type, new_var, x, y);
 }
