4e559be37bfa305aa9e67b0c07ab997e3ee91bca
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-29 17:00:38 -0500

    Include PDCurses in desktop app, add basic usage
    * Initializes PDCurses and reads user input in the game run
      callback function

--- AWO_App/Src/main.c
+++ AWO_App/Src/main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
 
 #include "AWO/Include/game.h"
 
+static WINDOW* curses_win;
+
 void game_run_callback(Game* game)
 {
+    // Read for character input, exit if none was found
+    char c;
+    if ((c = getch()) == ERR) {
+        return;
+    }
+
+    refresh();
 }
 
 int main(int argc, char** argv)
 {
+    // Initialize PDCurses
+    curses_win = initscr();
+
+    // Make PDCurses' `getch` immediately return ERR if no input is waiting
+    nodelay(curses_win, TRUE);
+
+    // Initialize game & run
     Game* game;
 
     if ((game = create_game(0, 0)) == NULL) {
@@ -22,6 +39,7 @@ int main(int argc, char** argv)
     }
 
     free_game(game);
+    endwin();
 
     return 0;
 }
