b4f9f5ec0559b1a70ca27aab5a6a21e0d531a52c
Kevin Pageau <pageau.kevin@gmail.com>
2019-12-31 19:08:11 -0500

    Make it possible to free game while running

--- AWO_Console/Src/Console/Functions/free.c
+++ AWO_Console/Src/Console/Functions/free.c
@@ -15,8 +15,15 @@ int free_console_game(Console* console, void* payload[CMD_ARG_MAX_COUNT])
         return CMD_Ret_Error;
     }
 
+    Game_State state = get_game_state(console->game);
+
     free_game(console->game);
     console->game = NULL;
 
-    return CMD_Ret_OK;
+    // If the game was running, return a ret value of 'Exit' so the game loop breaks
+    if (state == Game_Running) {
+        return CMD_Ret_Exit;
+    } else {
+        return CMD_Ret_OK;
+    }
 }
