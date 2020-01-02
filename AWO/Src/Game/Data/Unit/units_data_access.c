2f19eec70a586e0704b9ebff4c13750e3a863b84
Kevin Pageau <pageau.kevin@gmail.com>
2020-01-02 16:30:49 -0500

    Refactor units_data to use new JSON structure
    * Uses new game data JSON file structure to create/access
      the units data module

--- AWO/Src/Game/Data/Unit/units_data_access.c
+++ AWO/Src/Game/Data/Unit/units_data_access.c
@@ -15,20 +15,55 @@ Animation* get_unit_animation(
         ? variation
         : ARMY_TYPE_FIRST;
 
-    return units_data->src[type]->variations[returned_var][animation];
+    return units_data->src[type]->variations[returned_var]->animations[animation];
+}
+
+void free_unit_type_data(Unit_Type_Data* type_data)
+{
+    if (type_data== NULL) {
+        return;
+    }
+
+    if (type_data->variations == NULL) {
+        free(type_data);
+        return;
+    }
+
+    // Free all variations' data
+    for (int i = 0; i < type_data->variations_count; i++) {
+        if (type_data->variations[i] == NULL) {
+            continue;
+        }
+
+        for (Unit_Anim anim = UNIT_ANIM_FIRST; anim < UNIT_ANIM_COUNT; anim++) {
+            free_animation(type_data->variations[i]->animations[anim]);
+        }
+
+        free(type_data->variations[i]);
+    }
+
+    free(type_data->variations);
+    free(type_data);
 }
 
 void free_units_data(Units_Data* units_data)
 {
-    if (units_data != NULL) {
-        // TODO
-        free(units_data);
+    if (units_data == NULL) {
+        return;
     }
+
+    // Free all types' data
+    for (int i = 0; i < UNIT_TYPE_COUNT; i++) {
+        free_unit_type_data(units_data->src[i]);
+    }
+
+    free(units_data);
 }
 
 #ifdef _DEBUG
 void print_unit_type_data(Unit_Type_Data* unit_type_data)
 {
+    /*
     printf("Vars amount: %d\n", unit_type_data->variations_count);
 
     // Loop variations
@@ -41,13 +76,16 @@ void print_unit_type_data(Unit_Type_Data* unit_type_data)
             print_animation(unit_type_data->variations[var][anim]);
         }
     }
+    */
 }
 
 void print_units_data(Units_Data* units_data)
 {
+    /*
     for (Unit_Type type = UNIT_TYPE_FIRST; type <= UNIT_TYPE_LAST; type++) {
         printf("\n\n====================\n%s\n====================\n", unit_type_str[type]);
         print_unit_type_data(units_data->src[type]);
     }
+    */
 }
 #endif
