432ddda8f71a7dd52b3eeb4c5a3ed2093d9c7139
Kevin Pageau <pageau.kevin@gmail.com>
2020-01-15 18:18:27 -0500

    Make entity texture reader & game data public

--- AWO/Include/Entity_Texture_Reader/entity_texture_reader.h
+++ AWO/Include/Entity_Texture_Reader/entity_texture_reader.h
@@ -1,29 +1,41 @@
 #pragma once
 
-/*! @brief Reads the texture data from a game entity.
+#include "Include/types.h"
+#include "Include/Game_Data/game_data.h"
+
+/*! @brief Module used to retrieve texture data for individual entities, with palettes applied.
+ */
+typedef struct Entity_Texture_Reader Entity_Texture_Reader;
+
+/*! @brief Creates an entity texture reader.
  *
- *  @note The returned buffer must be freed by the user.
+ *  @param[in] game_data Reference to the game data module.
+ *  @return The created entity texture reader.
+ */
+Entity_Texture_Reader* create_entity_texture_reader(Game_Data* game_data);
+
+/*! @brief Reads the texture of a neutral tile and returns its pixel data in a new buffer.
  *
- *  @param[in] reader The entity texture reader instance.
- *  @param[in] kind The kind of the game entity to read from.
- *  @param[in] type The type of the entity. Tile_Type/Property_Type/Unit_Type
- *  @param[in] variation The variation of the entity. Determines the visual style to get.
- *  For neutral tiles: Tile_Variation. For properties & units: Army_Variation.
- *  @param[in] palette_variation The palette variation. Determines the colors applied.
- *  For neutral tiles: Weather. For properties & units: Army_Variation.
- *
- *  @param[out] texture_width The loaded texture's width.
- *  @param[out] texture_height The loaded texture's height.
- *  @return Buffer containing the entity's texture data.
+ *  @param[in] reader The entity texture reader.
+ *  @param[in] type The type of the tile to read.
+ *  @param[in] variation The variation of the tile to read.
+ *  @param[in] weather The weather palette to apply to the tile.
+ *  @param[out] texture_w Filled in with the width of the read texture.
+ *  @param[out] texture_h Filled in with the height of the read texture.
  */
 /*
-unsigned char* read_game_entity_texture(
+Uint8* read_neutral_tile_texture(
     Entity_Texture_Reader* reader,
-    Entity_Kind kind,
-    int type,
-    int variation,
-    int palette_variation,
-    int* texture_width,
-    int* texture_height
+    Tile_Type type,
+    Tile_Variation variation,
+    Weather weather,
+    int* texture_w,
+    int* texture_h
 );
 */
+
+/*! @brief Frees an entity texture reader's memory.
+ *
+ *  @param[in] reader The reader to free.
+ */
+void free_entity_texture_reader(Entity_Texture_Reader* reader);
---
+++ AWO/Libs/Entity_Texture_Reader/entity_texture_reader.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "Include/Entity_Texture_Reader/entity_texture_reader.h"
+#include "Game_Data/game_data.h"
