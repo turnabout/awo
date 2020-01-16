c677b22a74cd2c28bdf1c64d7f9cbfe43a229624
Kevin Pageau <pageau.kevin@gmail.com>
2020-01-15 20:16:40 -0500

    Remove unnecessary funcs from game messages simple

--- AWO/Libs/Game_Message/game_message_simple.c
+++ AWO/Libs/Game_Message/game_message_simple.c
@@ -5,10 +5,6 @@
 
 #include "Game_Message/game_message.h"
 
-void start_message_service()
-{
-}
-
 void push_msg(Game_Message_Label label, char* format, ...)
 {
     va_list a_ptr;
@@ -16,15 +12,6 @@ void push_msg(Game_Message_Label label, char* format, ...)
     va_start(a_ptr, format);
     printf("%s\n", format, a_ptr);
     va_end(a_ptr);
-}
-
-Game_Message* pop_msg()
-{
-    return NULL;
-}
-
-void end_message_service()
-{
 }
 
 #endif
