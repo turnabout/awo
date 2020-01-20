827d6a4dffc10d976a5181fd48312893b83ac78e
Kevin Pageau <pageau.kevin@gmail.com>
2020-01-20 11:14:19 -0500

    Add AWO_Desktop project for barebones AWO testing

---
+++ AWO_Desktop/Src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <AWO/Include/awo.h>
+
+int test_cb(void* value)
+{
+    return 0;
+}
+
+int main()
+{
+    Game_Data* data = create_game_data();
+    Game_Window* window = create_game_window(0, 0);
+    Game* game = create_editor_game(data, window);
+
+    show_game_window(window);
+    run_game(game, window, test_cb, NULL);
+    hide_game_window(window);
+
+    free_game_data(data);
+    free_game_window(window);
+    free_editor_game(game);
+}
